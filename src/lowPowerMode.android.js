46926376b4f1770b05eba0b0f0c3880d48094d78
Marc Rousavy <marcrousavy@hotmail.com>
2021-01-20 14:51:27 +0100

    Update lowPowerMode.android.js

--- src/lowPowerMode.android.js
+++ src/lowPowerMode.android.js
@@ -1,5 +1,5 @@
 export default {
-    isLowPowerModeEnabled: () => false,
+    isLowPowerModeEnabled: () => Promise.resolve(false),
     setLowPowerModeListener: () => {},
     removeLowPowerModeListener: () => {}
 };
